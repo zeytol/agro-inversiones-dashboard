fcd25399508b17cd20b7d0796ab72834be4bf98f
eleazar caqui <eleazar969mejia@gmail.com>
2024-10-23 09:36:06 -0500

    subiendo la creación del proyecto agro inversiones para frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { AuthenticationComponent } from './core/authentication/authentication.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AuthenticationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
