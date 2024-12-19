2f3304b9571107b345f3e664649d7e08c06e079b
eleazar caqui <eleazar969mejia@gmail.com>
2024-12-19 17:31:25 -0500

    actulización de la carpeta dist v03

---
+++ dist/agro-inversiones/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-6AHLRADT.mjs";import"./chunk-GN3HINFO.mjs";import"./chunk-MBNVXL5U.mjs";import"./chunk-AZLUZVVY.mjs";import"./chunk-E44LSF4W.mjs";export{a as default};
