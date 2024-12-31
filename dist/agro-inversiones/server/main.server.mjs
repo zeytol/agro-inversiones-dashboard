e37a5a7974c7bc973309cbe490624aa60a8a1810
eleazar caqui <eleazar969mejia@gmail.com>
2024-12-31 12:48:29 -0500

    implementación de modal editar de productos

--- dist/agro-inversiones/server/main.server.mjs
+++ dist/agro-inversiones/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OHV27OWX.mjs";import"./chunk-GN3HINFO.mjs";import"./chunk-MBNVXL5U.mjs";import"./chunk-AZLUZVVY.mjs";import"./chunk-E44LSF4W.mjs";export{a as default};
+import{a}from"./chunk-GQU62JWD.mjs";import"./chunk-GN3HINFO.mjs";import"./chunk-MBNVXL5U.mjs";import"./chunk-AZLUZVVY.mjs";import"./chunk-E44LSF4W.mjs";export{a as default};
